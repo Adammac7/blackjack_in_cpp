fb8b0b73220202f336da5fafb590ea16eb83521e
Adammac7 <adammac2004@gmail.com>
2025-12-03 13:48:15 -0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+/*
+TO RUN THIS CODE:
+
+# compile
+g++ -std=c++17 -O2 -Wall -Wextra -g -o src/main.exe src/main.cpp
+
+# run
+.\src\main.exe
+
+*/
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  
+    string welcome_string = "Welcome to Blackjack\n";
+    cout << welcome_string;
+
+    int buy_in;
+    cout << "Enter your buy-in amount: ";
+    cin >> buy_in;
+    cout << "You have bought in for $" << buy_in << "\n";
+
+    return 0;
+}
